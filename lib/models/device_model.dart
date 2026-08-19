9bc7a7533aef20e5f7ef3fc5b94cc89366f39fb1
ahmedelzae2000-cmyk <ahmedelzae2000@gmail.com>
2026-08-19 20:02:39 +0300

    Rename DeviceModel to Device and modify PaymentMethod
    Refactored DeviceModel to Device and updated PaymentMethod enum.

--- lib/models/device_model.dart
+++ lib/models/device_model.dart
@@ -1,43 +1,16 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 enum GameMode { single, multi }
-enum PaymentMethod { cash, vodafone }
+enum PaymentMethod { cash, visa }
 
-class DeviceModel {
+class Device {
   final String id;
   final String name;
   final String type;
-  final double singleRate;
-  final double multiRate;
   bool isOccupied;
-  GameMode mode;
-  PaymentMethod paymentMethod;
-  DateTime? startTime;
 
-  DeviceModel({
+  Device({
     required this.id,
     required this.name,
     required this.type,
-    required this.singleRate,
-    required this.multiRate,
     this.isOccupied = false,
-    this.mode = GameMode.single,
-    this.paymentMethod = PaymentMethod.cash,
-    this.startTime,
   });
-
-  factory DeviceModel.fromFirestore(DocumentSnapshot doc) {
-    final data = doc.data() as Map<String, dynamic>? ?? {};
-    return DeviceModel(
-      id: doc.id,
-      name: data['name'] ?? '',
-      type: data['type'] ?? 'PS4',
-      singleRate: (data['singleRate'] as num?)?.toDouble() ?? 20.0,
-      multiRate: (data['multiRate'] as num?)?.toDouble() ?? 30.0,
-      isOccupied: data['isOccupied'] ?? false,
-      mode: data['mode'] == 'multi' ? GameMode.multi : GameMode.single,
-      paymentMethod: data['paymentMethod'] == 'vodafone' ? PaymentMethod.vodafone : PaymentMethod.cash,
-      startTime: data['startTime'] != null ? (data['startTime'] as Timestamp).toDate() : null,
-    );
-  }
 }
